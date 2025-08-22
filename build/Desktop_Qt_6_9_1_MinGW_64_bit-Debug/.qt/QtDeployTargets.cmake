53c4921c36b998cfcaac9407444b5072a8d15b3e
wint3r <pcl2678683235@gmail.com>
2025-08-22 09:56:43 +0800

    remove build folder

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MQTT_UI_FILE E:/QT_Projects/MQTT_UI/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/MQTT_UI.exe)
-set(__QT_DEPLOY_TARGET_MQTT_UI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MQTT_UI_RUNTIME_DLLS D:/APP/QT/6.9.1/mingw_64/bin/Qt6Quick.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6QmlMeta.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6QmlWorkerScript.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6QmlModels.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6Qml.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6OpenGL.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6Gui.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6Network.dll;D:/APP/QT/6.9.1/mingw_64/bin/Qt6Core.dll)
